e56cc4c5167d5eb3b0eba2aaba72794f1f40bc3e
Niell-A <122717455+MayoHotSauce@users.noreply.github.com>
2023-01-18 14:46:15 +0700

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,14 @@
+#include <conio.h>
+#include <iostream>
+
+head(char jud[50]=""){
+	cout<<"***************************************************************"<<endl;
+   cout<<".....................::LOGIN APP::............................."<<endl;
+	cout<<"                          "<<jud<<endl;
+  	cout<<"***************************************************************"<<endl;
+    	cout<<" NAMA      : A- Niel"<<endl;
+   	cout<<" KELAS     : 10 PPLG"<<endl;
+   	cout<<" ALAMAT    : XXXXXXXXX"<<endl;
+      cout<<" HOBI      : XXXXXXXXX"<<endl;
+   	cout<<"==============================================================="<<endl;
+  }
